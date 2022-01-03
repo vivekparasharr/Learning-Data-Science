392e0f7287a221b15c06bbab10584ccaa326428e
Vivek Parashar <38409647+vivekparasharr@users.noreply.github.com>
2022-01-02 22:47:29 -0500

    x

--- ml.R
+++ ml.R
@@ -616,19 +616,8 @@ points(set, pch = 21, bg = ifelse(set[, 3] == 2, 'blue3', ifelse(set[, 3] == 1, 
 # LDA
 
 # Importing the dataset
-dataset = read.csv('Wine.csv')
-
 # Splitting the dataset into the Training set and Test set
-# install.packages('caTools')
-library(caTools)
-set.seed(123)
-split = sample.split(dataset$Customer_Segment, SplitRatio = 0.8)
-training_set = subset(dataset, split == TRUE)
-test_set = subset(dataset, split == FALSE)
-
 # Feature Scaling
-training_set[-14] = scale(training_set[-14])
-test_set[-14] = scale(test_set[-14])
 
 # Applying LDA
 library(MASS)
@@ -639,49 +628,10 @@ test_set = as.data.frame(predict(lda, test_set))
 test_set = test_set[c(5, 6, 1)]
 
 # Fitting SVM to the Training set
-# install.packages('e1071')
-library(e1071)
-classifier = svm(formula = class ~ .,
-                 data = training_set,
-                 type = 'C-classification',
-                 kernel = 'linear')
-
 # Predicting the Test set results
-y_pred = predict(classifier, newdata = test_set[-3])
-
 # Making the Confusion Matrix
-cm = table(test_set[, 3], y_pred)
-
 # Visualising the Training set results
-library(ElemStatLearn)
-set = training_set
-X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
-X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
-grid_set = expand.grid(X1, X2)
-colnames(grid_set) = c('x.LD1', 'x.LD2')
-y_grid = predict(classifier, newdata = grid_set)
-plot(set[, -3],
-     main = 'SVM (Training set)',
-     xlab = 'LD1', ylab = 'LD2',
-     xlim = range(X1), ylim = range(X2))
-contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
-points(grid_set, pch = '.', col = ifelse(y_grid == 2, 'deepskyblue', ifelse(y_grid == 1, 'springgreen3', 'tomato')))
-points(set, pch = 21, bg = ifelse(set[, 3] == 2, 'blue3', ifelse(set[, 3] == 1, 'green4', 'red3')))
-
 # Visualising the Test set results
-library(ElemStatLearn)
-set = test_set
-X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
-X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
-grid_set = expand.grid(X1, X2)
-colnames(grid_set) = c('x.LD1', 'x.LD2')
-y_grid = predict(classifier, newdata = grid_set)
-plot(set[, -3], main = 'SVM (Test set)',
-     xlab = 'LD1', ylab = 'LD2',
-     xlim = range(X1), ylim = range(X2))
-contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
-points(grid_set, pch = '.', col = ifelse(y_grid == 2, 'deepskyblue', ifelse(y_grid == 1, 'springgreen3', 'tomato')))
-points(set, pch = 21, bg = ifelse(set[, 3] == 2, 'blue3', ifelse(set[, 3] == 1, 'green4', 'red3')))
 
 
 
@@ -691,20 +641,8 @@ points(set, pch = 21, bg = ifelse(set[, 3] == 2, 'blue3', ifelse(set[, 3] == 1, 
 # Kernel PCA
 
 # Importing the dataset
-dataset = read.csv('Social_Network_Ads.csv')
-dataset = dataset[, 3:5]
-
 # Splitting the dataset into the Training set and Test set
-# install.packages('caTools')
-library(caTools)
-set.seed(123)
-split = sample.split(dataset$Purchased, SplitRatio = 0.75)
-training_set = subset(dataset, split == TRUE)
-test_set = subset(dataset, split == FALSE)
-
 # Feature Scaling
-training_set[, 1:2] = scale(training_set[, 1:2])
-test_set[, 1:2] = scale(test_set[, 1:2])
 
 # Applying Kernel PCA
 # install.packages('kernlab')
@@ -716,53 +654,11 @@ test_set_pca = as.data.frame(predict(kpca, test_set))
 test_set_pca$Purchased = test_set$Purchased
 
 # Fitting Logistic Regression to the Training set
-classifier = glm(formula = Purchased ~ .,
-                 family = binomial,
-                 data = training_set_pca)
-
 # Predicting the Test set results
-prob_pred = predict(classifier, type = 'response', newdata = test_set_pca[-3])
-y_pred = ifelse(prob_pred > 0.5, 1, 0)
-
 # Making the Confusion Matrix
-cm = table(test_set_pca[, 3], y_pred)
-
 # Visualising the Training set results
-install.packages('ElemStatLearn')
-library(ElemStatLearn)
-set = training_set_pca
-X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
-X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
-grid_set = expand.grid(X1, X2)
-colnames(grid_set) = c('V1', 'V2')
-prob_set = predict(classifier, type = 'response', newdata = grid_set)
-y_grid = ifelse(prob_set > 0.5, 1, 0)
-plot(set[, -3],
-     main = 'Logistic Regression (Training set)',
-     xlab = 'PC1', ylab = 'PC2',
-     xlim = range(X1), ylim = range(X2))
-contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
-points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
-points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
-
 # Visualising the Test set results
 # install.packages('ElemStatLearn')
-library(ElemStatLearn)
-set = test_set_pca
-X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
-X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
-grid_set = expand.grid(X1, X2)
-colnames(grid_set) = c('V1', 'V2')
-prob_set = predict(classifier, type = 'response', newdata = grid_set)
-y_grid = ifelse(prob_set > 0.5, 1, 0)
-plot(set[, -3],
-     main = 'Logistic Regression (Test set)',
-     xlab = 'Age', ylab = 'Estimated Salary',
-     xlim = range(X1), ylim = range(X2))
-contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
-points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
-points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
-
 
 
 
--- ml.py
+++ ml.py
@@ -614,7 +614,6 @@ plt.show()
 
 # Principal Component Analysis (PCA)
 
-
 # Importing the dataset
 dataset = pd.read_csv('data/09_Wine.csv')
 X = dataset.iloc[:, :-1].values
@@ -684,24 +683,9 @@ plt.show()
 # Linear Discriminant Analysis (LDA)
 
 # Importing the libraries
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-
 # Importing the dataset
-dataset = pd.read_csv('data/09_Wine.csv')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, -1].values
-
 # Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
-
 # Feature Scaling
-from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
 
 # Applying LDA
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
@@ -710,78 +694,18 @@ X_train = lda.fit_transform(X_train, y_train)
 X_test = lda.transform(X_test)
 
 # Training the Logistic Regression model on the Training set
-from sklearn.linear_model import LogisticRegression
-classifier = LogisticRegression(random_state = 0)
-classifier.fit(X_train, y_train)
-
 # Making the Confusion Matrix
-from sklearn.metrics import confusion_matrix, accuracy_score
-y_pred = classifier.predict(X_test)
-cm = confusion_matrix(y_test, y_pred)
-print(cm)
-accuracy_score(y_test, y_pred)
-
 # Visualising the Training set results
-from matplotlib.colors import ListedColormap
-X_set, y_set = X_train, y_train
-X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
-                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green', 'blue')))
-plt.xlim(X1.min(), X1.max())
-plt.ylim(X2.min(), X2.max())
-for i, j in enumerate(np.unique(y_set)):
-    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                c = ListedColormap(('red', 'green', 'blue'))(i), label = j)
-plt.title('Logistic Regression (Training set)')
-plt.xlabel('LD1')
-plt.ylabel('LD2')
-plt.legend()
-plt.show()
-
 # Visualising the Test set results
-from matplotlib.colors import ListedColormap
-X_set, y_set = X_test, y_test
-X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
-                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green', 'blue')))
-plt.xlim(X1.min(), X1.max())
-plt.ylim(X2.min(), X2.max())
-for i, j in enumerate(np.unique(y_set)):
-    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                c = ListedColormap(('red', 'green', 'blue'))(i), label = j)
-plt.title('Logistic Regression (Test set)')
-plt.xlabel('LD1')
-plt.ylabel('LD2')
-plt.legend()
-plt.show()
-
-
 
 
 
 # Kernel PCA
 
 # Importing the libraries
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-
 # Importing the dataset
-dataset = pd.read_csv('data/09_Wine.csv')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, -1].values
-
 # Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
-
 # Feature Scaling
-from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
 
 # Applying Kernel PCA
 from sklearn.decomposition import KernelPCA
@@ -790,8 +714,38 @@ X_train = kpca.fit_transform(X_train)
 X_test = kpca.transform(X_test)
 
 # Training the Logistic Regression model on the Training set
-from sklearn.linear_model import LogisticRegression
-classifier = LogisticRegression(random_state = 0)
+# Making the Confusion Matrix
+# Visualising the Training set results
+# Visualising the Test set results
+
+
+
+
+#########################################################
+########### MODEL SELECTION AND BOOSING #################
+#########################################################
+
+
+# k-Fold Cross Validation
+
+# Importing the dataset
+dataset = pd.read_csv('data/10_Social_Network_Ads.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Training the Kernel SVM model on the Training set
+from sklearn.svm import SVC
+classifier = SVC(kernel = 'rbf', random_state = 0)
 classifier.fit(X_train, y_train)
 
 # Making the Confusion Matrix
@@ -801,21 +755,27 @@ cm = confusion_matrix(y_test, y_pred)
 print(cm)
 accuracy_score(y_test, y_pred)
 
+# Applying k-Fold Cross Validation
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
+print("Accuracy: {:.2f} %".format(accuracies.mean()*100))
+print("Standard Deviation: {:.2f} %".format(accuracies.std()*100))
+
 # Visualising the Training set results
 from matplotlib.colors import ListedColormap
 X_set, y_set = X_train, y_train
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
                      np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
 plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green', 'blue')))
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
 plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                c = ListedColormap(('red', 'green', 'blue'))(i), label = j)
-plt.title('Logistic Regression (Training set)')
-plt.xlabel('PC1')
-plt.ylabel('PC2')
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Kernel SVM (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
 plt.legend()
 plt.show()
 
@@ -825,27 +785,17 @@ X_set, y_set = X_test, y_test
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
                      np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
 plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green', 'blue')))
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
 plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                c = ListedColormap(('red', 'green', 'blue'))(i), label = j)
-plt.title('Logistic Regression (Test set)')
-plt.xlabel('PC1')
-plt.ylabel('PC2')
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Kernel SVM (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
 plt.legend()
 plt.show()
-
-
-
-
-
-
-#########################################################
-########### MODEL SELECTION AND BOOSING #################
-#########################################################
-
 
 
 
