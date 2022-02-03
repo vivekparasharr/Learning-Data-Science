87d0952ca0d858eb61e5e658a884916ef192a98a
Vivek Parashar <38409647+vivekparasharr@users.noreply.github.com>
2022-02-02 20:01:49 -0500

    Update dl.py

--- dl.py
+++ dl.py
@@ -15,7 +15,7 @@ dataset = pd.read_csv('data/01_pre_processing_data.csv')
 # Separating independent and dependent variables
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
-
+ 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
